c2cadc608b6d8cee519689fa73b5b88cd048806a
sibabale <sibabalejoja@gmail.com>
2023-11-24 11:33:42 +0200

    style: housekeeping

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -12,18 +12,10 @@ class ProjectController extends Controller
 
     public function index()
     {
-        // Get all projects
         $projects = Project::all();
 
-        // Return a view or JSON response as needed
         return response()->json($projects);
 
-    }
-
-    public function create()
-    {
-        // Using Inertia.js to return an Inertia response
-        // return Inertia::render('Projects/Create');
     }
 
     public function show($projectId)
@@ -36,6 +28,8 @@ class ProjectController extends Controller
             return response()->json(['message' => 'Project not found'], 404);
         }
     }
+
+
     public function store(Request $request)
     {
         $request->validate([
@@ -48,7 +42,6 @@ class ProjectController extends Controller
 
         $user = Auth::user();
 
-        // Create a new project with form inputs
         $project = new Project([
             'name' => $request->input('name'),
             'description' => $request->input('description'),
@@ -59,14 +52,11 @@ class ProjectController extends Controller
 
         $user->projects()->save($project);
 
-         // Return a view or JSON response as needed
-        return response()->json($project, 201);
-        // return redirect()->route('projects.index');
+        return response()->json($project);
     }
 
     public function update(Request $request, $id)
     {
-        // Validate the request data
         $validatedData = $request->validate([
             'name' => 'string|max:255',
             'description' => 'string',
@@ -75,25 +65,19 @@ class ProjectController extends Controller
             'database' => 'string',
         ]);
 
-        // Update the project
         $project = Project::findOrFail($id);
         $project->update($validatedData);
 
-        // Return a view or JSON response as needed
         return response()->json($project);
-        // return redirect()->route('projects.index');
 
     }
 
     public function destroy($id)
     {
-        // Delete a project by ID
         $project = Project::findOrFail($id);
         $project->delete();
 
-        // Return a view or JSON response as needed
         return response()->json(null, 204);
-        // return redirect()->route('projects.index');
     }
 
     public function edit($id)
@@ -101,10 +85,5 @@ class ProjectController extends Controller
         $project = Project::findOrFail($id);
 
         return response()->json(project, 201);
-
-        // Using Inertia.js to return an Inertia response
-        // return Inertia::render('Projects/Edit', [
-        //     'project' => $project,
-        // ]);
     }
 }
