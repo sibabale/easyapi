2958c38ff36f91c8ce7f7b34b087b2e76657dc20
sibabale <sibabalejoja@gmail.com>
2023-12-03 14:40:15 +0200

    refactor: added a link

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,9 +4,14 @@
     <div class="flex">
       <aside class="w-1/5 border-r-2 border-r-gray-100 h-screen">
         <h1 class="p-5 text-2xl font-bold">Easy API</h1>
+        <ul class="px-5">
+          <li>
+            <router-link to="/projects">Projects</router-link>
+          </li>
+        </ul>
       </aside>
       <div class="flex flex-col w-full">
-        <nav class="p-5 border-b-2 border-b-gray-100">Navbar</nav>
+        <nav class="p-5 border-b-2 border-b-gray-100"></nav>
         <main class="p-5">
           <router-view></router-view>
         </main>
