b31c6f4340e09efce202a05e9a23531dee2646ec
sibabale <sibabalejoja@gmail.com>
2023-12-03 15:58:35 +0200

    empty state

---
+++ client/src/components/EmptyState.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="p-5 my-5 border border-dashed rounded-lg text-center">
+    {{ props.message }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { defineProps } from 'vue'
+
+const props = defineProps<{ message: string }>()
+</script>
